6f8de763437ceedc4c0c0c347798f76787931c4f
madu <160867324+marialapinski14@users.noreply.github.com>
2024-07-15 11:07:48 -0300

    atividade respondida

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Menu mobile
-
+function toggleMenu(){
+  const nav = document.querySelector(`nav`)
+  nav.classList.toggle(`mobile`)
+}
 // Imagem 1
 const imagem1 = document.getElementById("lampada-01");
 imagem1.addEventListener("click", function () {
@@ -12,3 +15,12 @@ imagem1.addEventListener("click", function () {
 });
 
 // Imagem 2
+const imagem2 = document.getElementById("lampada-02");
+imagem2.addEventListener("mouseover", function () {
+  imagem2.src = "img/lampada-on.jpg";
+});
+  imagem2.addEventListener("mouseout", function () {
+    imagem2.src = "img/lampada.jpg";
+    
+  
+});
